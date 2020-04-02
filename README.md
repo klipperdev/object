a3636e55560c685ed3f0ed98cfb6f640b81f21fc
François Pluchino <francois.pluchino@klipper.dev>
2020-04-02 18:51:53 +0200

    Transfer object component to Klipper

---
+++ README.md
@@ -0,0 +1,12 @@
+Klipper Object Component
+========================
+
+The Object component is a set of utils for the objects.
+
+Resources
+---------
+
+- [Documentation](https://doc.klipper.dev/components/object)
+- [Report issues](https://github.com/klipperdev/klipper/issues)
+  and [send Pull Requests](https://github.com/klipperdev/klipper/pulls)
+  in the [main Klipper repository](https://github.com/klipperdev/klipper)
---
+++ Util/ClassUtil.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Component\Object\Util;
+
+/**
+ * Class Utils.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+abstract class ClassUtil
+{
+    /**
+     * Check the object or class name is instance of the class name.
+     *
+     * @param object|\ReflectionClass|string $object The object instance or the reflection class or the class name
+     * @param string                         $class  The class name
+     *
+     * @return bool
+     */
+    public static function isInstanceOf($object, string $class): bool
+    {
+        if ($object instanceof \ReflectionClass) {
+            $object = $object->getName();
+        }
+
+        return is_a($object, $class, true);
+    }
+
+    /**
+     * Check the object or class name is one instances of the class names.
+     *
+     * @param object|\ReflectionClass|string $object  The object instance or the reflection class or the class name
+     * @param string[]                       $classes The class names
+     *
+     * @return bool
+     */
+    public static function isOneInstancesOf($object, array $classes): bool
+    {
+        foreach ($classes as $class) {
+            if (static::isInstanceOf($object, $class)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
